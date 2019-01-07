675402110a64d2543e7e62e910fe7f29b475bbdd
thaitai <phamthaitai2303@gmail.com>
2019-01-07 23:36:20 +0700

    Junit

--- src/PageObject/Object.java
+++ src/PageObject/Object.java
@@ -3,4 +3,8 @@ package PageObject;
 public class Object {
 
     String bt_login = "/html/body/header/div/div/div/div[4]/div/div/a";
+    String email_textbox = ".//input[@id='e_p_lg' and @name='e_p_lg']";
+    String pass_textbox = ".//input[@id='pass_lg' and @name='pass_lg']";
+    String Login_bt = ".//input[@id='submit_lg' and @name='login']";
+    String error_login = ".//i[@class='text-danger']";
 }
